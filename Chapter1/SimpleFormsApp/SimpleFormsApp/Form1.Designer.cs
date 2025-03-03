729840f3d4f4c4c324153349d4baf963c8d9a72a
KiaKassandraGalamiton <galamiton.kia357@gmail.com>
2025-03-02 23:40:19 -0800

    Add files via upload

---
+++ Chapter1/SimpleFormsApp/SimpleFormsApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace SimpleFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label3 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(73, 168);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(229, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(73, 243);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(229, 23);
+            textBox5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Book Antiqua", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(74, 377);
+            button1.Name = "button1";
+            button1.Size = new Size(228, 28);
+            button1.TabIndex = 5;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(73, 150);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 15);
+            label1.TabIndex = 6;
+            label1.Text = "First Name";
+            label1.Click += label1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Book Antiqua", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(74, 225);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 16);
+            label3.TabIndex = 8;
+            label3.Text = "Last Name";
+            label3.Click += label3_Click;
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(74, 32);
+            label5.Name = "label5";
+            label5.Size = new Size(390, 42);
+            label5.TabIndex = 10;
+            label5.Text = "SIMPLE FORM APPLICATION";
+            label5.Click += label5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox5;
+        private Button button1;
+        private Label label1;
+        private Label label3;
+        private Label label5;
+    }
+}
---
+++ Chapter1/SimpleFormsApp/SimpleFormsApp/Form1.cs
@@ -0,0 +1,47 @@
+namespace SimpleFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fnamevalue = textBox1.Text;
+            string lnamevalue = textBox5.Text;
+
+            MessageBox.Show($" Good Day {fnamevalue} {lnamevalue}!", "Greetings", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
