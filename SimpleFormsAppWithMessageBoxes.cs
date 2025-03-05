1ff5064b4cd7a78eafe24157a0c8349fd7cbb947
KiaKassandraGalamiton <galamiton.kia357@gmail.com>
2025-03-04 23:55:29 -0800

    Add files via upload

---
+++ SimpleFormsAppWithMessageBoxes.cs
@@ -0,0 +1,57 @@
+namespace SimpleFormsAppWithMessageBoxes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Submit Information?",
+                "Confirmation", MessageBoxButtons.YesNo);
+            switch (dr)
+            {
+                case DialogResult.Yes:
+                    break;
+                case DialogResult.No:
+                    break;
+            }
+
+            {
+                string fNameValue = firstName.Text;
+                string mNameValue = middleName.Text;
+                string lNameValue = lastName.Text;
+                string suffixValue = suffix.Text;
+                MessageBox.Show($"Hi! {fNameValue} {mNameValue} {lNameValue} {suffixValue}", "Greetings!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firstName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
